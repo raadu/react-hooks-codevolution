9807dab6cde151658edb983a5a07656d16308b44
raadu <raiyad.raad@gmail.com>
2020-12-21 18:39:27 +0600

    upto lesson 13 codes added

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 
 const DataFetching = () => {
     const [posts, setPosts] = useState([]);
+    const [id, setId] = useState(1);
 
     useEffect(() => {
-        axios 
-        .get('https://jsonplaceholder.typicode.com/posts')
+        axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then((res) => {
             console.log(res);
             setPosts(res.data);
@@ -14,19 +15,22 @@ const DataFetching = () => {
         .catch((err) => {
           console.log("Error: ", err);
         })
-    },[]);
+    },[id]);
+
     return (
-        <div>
-            <ul>
-                {posts.map((post) => {
-                    return(
-                        <li key={posts.id}>
-                            {post.title}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+      <div>
+        <input 
+            type="text" 
+            value={id} 
+            onChange={(e) => setId(e.target.value)} 
+        />
+        <div>{posts.title}</div>
+        {/* <ul>
+          {posts.map((post) => {
+            return <li key={posts.id}>{post.title}</li>;
+          })}
+        </ul> */}
+      </div>
     );
 }
  
